359be551c8c75cb9d9822247a512e9d3aee342a8
Chris Yates <chris@clyates.plus.com>
2023-06-09 23:26:29 +0100

    Use exception message class constants Use Attributes Refactor data providers to Generators

--- tests/N6lPhoneNumberDataTest.php
+++ tests/N6lPhoneNumberDataTest.php
@@ -6,58 +6,53 @@
 
 declare(strict_types=1);
 
-namespace Tests;
+namespace BeastBytes\PhoneNumber\N6l\PHP\Tests;
 
 use BeastBytes\PhoneNumber\N6l\PHP\N6lPhoneNumberData;
+use PHPUnit\Framework\Attributes\BeforeClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use InvalidArgumentException;
+use RuntimeException;
 
 class N6LPhoneNumberDataTest extends TestCase
 {
-    private static array $goodCountries;
     private static array $n6lPhoneNumberData;
     private static N6lPhoneNumberData $testClass;
 
-    /**
-     * @beforeClass
-     */
+    #[BeforeClass]
     public static function init(): void
     {
-        self::$n6lPhoneNumberData = require dirname(__DIR__) . '/src/data.php';
         self::$testClass = new N6lPhoneNumberData();
     }
 
     public function test_getting_countries()
     {
-        $this->assertCount(count(self::$goodCountries), self::$testClass->getCountries());
+        $this->assertCount(
+            count(require dirname(__DIR__) . '/data/data.php'),
+            self::$testClass->getCountries()
+        );
     }
 
-    /**
-     * @dataProvider goodCountries
-     */
+    #[DataProvider('goodCountriesProvider')]
     public function test_has_country($country)
     {
         $this->assertTrue(self::$testClass->hasCountry($country));
     }
 
-    /**
-     * @dataProvider badCountries
-     */
+    #[DataProvider('badCountriesProvider')]
     public function test_does_not_have_country($country)
     {
         $this->assertFalse(self::$testClass->hasCountry($country));
     }
 
-    /**
-     * @dataProvider goodCountries
-     */
-    public function test_get_pattern($country, $hasReplacement)
+    #[DataProvider('goodCountriesProvider')]
+    public function test_get_pattern($country)
     {
         $this->assertSame(self::$n6lPhoneNumberData[$country]['pattern'], self::$testClass->getPattern($country));
     }
 
-    /**
-     * @dataProvider goodCountries
-     */
+    #[DataProvider('goodCountriesProvider')]
     public function test_replacement($country, $hasReplacement)
     {
         $this->assertSame($hasReplacement, self::$testClass->hasReplacement($country));
@@ -65,51 +60,63 @@ class N6LPhoneNumberDataTest extends TestCase
         if ($hasReplacement) {
             $this->assertSame(self::$n6lPhoneNumberData[$country]['replacement'], self::$testClass->getReplacement($country));
         } else {
-            $this->expectException(\RuntimeException::class);
-            $this->expectExceptionMessage("Country $country does not have a replacement");
+            $this->expectException(RuntimeException::class);
+            $this->expectExceptionMessage(strtr(
+                N6lPhoneNumberData::COUNTRY_DOES_NOT_HAVE_A_REPLACEMENT_EXCEPTION_MESSAGE,
+                ['{country}' => $country]
+            ));
             self::$testClass->getReplacement($country);
         }
     }
 
-    /**
-     * @dataProvider badCountries
-     */
+    #[DataProvider('badCountriesProvider')]
     public function test_bad_countries($country)
     {
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Country $country not found in list of national phone number formats");
+        $this->assertFalse(self::$testClass->hasCountry($country));
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(strtr(
+            N6lPhoneNumberData::COUNTRY_NOT_FOUND_EXCEPTION_MESSAGE,
+            ['{country}' => $country]
+        ));
         self::$testClass->getPattern($country);
 
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Country $country not found in list of national phone number formats");
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(strtr(
+            N6lPhoneNumberData::COUNTRY_NOT_FOUND_EXCEPTION_MESSAGE,
+            ['{country}' => $country]
+        ));
         self::$testClass->hasReplacement($country);
 
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Country $country not found in list of national phone number formats");
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(strtr(
+            N6lPhoneNumberData::COUNTRY_NOT_FOUND_EXCEPTION_MESSAGE,
+            ['{country}' => $country]
+        ));
         self::$testClass->getReplacement($country);
     }
 
-    public function goodCountries(): array
+    public static function goodCountriesProvider(): \Generator
     {
-        $n6lPhoneNumberData = require dirname(__DIR__) . '/src/data.php';
-        $goodCountries = [];
+        self::$n6lPhoneNumberData = require dirname(__DIR__) . '/data/data.php';
 
-        foreach (array_keys($n6lPhoneNumberData) as $country) {
-            $goodCountries[] = [$country, count($n6lPhoneNumberData[$country]) === 2];
+        foreach (array_keys(self::$n6lPhoneNumberData) as $country) {
+            yield $country => [$country, count(self::$n6lPhoneNumberData[$country]) === 2];
         }
-
-        self::$goodCountries = $goodCountries;
-        return self::$goodCountries;
     }
 
-    public function badCountries(): array
+    public static function badCountriesProvider(): \Generator
     {
-        return [
-            'non-existent code' => ['XX'],
-            'alpha-3 code' => ['GBR'],
-            'too short' => ['G'],
-            'too long' => ['GBRT'],
-            'number string' => ['12']
-        ];
+        foreach (
+            [
+                'non-existent code' => ['XX'],
+                'alpha-3 code' => ['GBR'],
+                'too short' => ['G'],
+                'too long' => ['GBRT'],
+                'number string' => ['12']
+            ] as $name => $value
+        ) {
+            yield $name => $value;
+        }
     }
 }
